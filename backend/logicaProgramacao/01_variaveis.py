3506614a340226c1baae39c18bc5c286834aab76
porjin2k24 <playertree333@gmail.com>
2025-06-17 09:47:51 -0300

    trabalhando com variaveis

---
+++ backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,25 @@
+nome ="Enzo"
+idade = 18
+altura = 1.83
+tem_carteira = False
+
+
+print("nome:", nome)
+print("idade:", idade)
+print("altura", altura)
+print("tem_carteira", tem_carteira)
+
+
+
+campeao ="yone"
+lancamento ="2020"
+kda ="5.6"
+e_meio = True
+
+
+print("campeão", campeao)
+print("lançamento", lancamento)
+print("kda", kda)
+print("é meio", e_meio)
+
+
