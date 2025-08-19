6890a70f143176c8af82c0e7ce065c1f72d6b9b8
porjin2k24 <playertree333@gmail.com>
2025-08-19 09:46:28 -0300

    lebron>>>>curry

---
+++ backend/logicaProgramacao/15_Atividade.py
@@ -0,0 +1,17 @@
+alunos = []
+
+alunos.append("Enzo")
+alunos.append("julio")
+alunos.append("joão")
+alunos.append("josé")
+alunos.append("caetano")
+alunos.append("maria")
+alunos.append("ana")
+alunos.append("sergio")
+alunos.append("anastacia")
+alunos.append("celestina")
+
+print (alunos)
+
+for aluno in alunos:
+    print("olá", aluno)
