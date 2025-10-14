62cc9e0bbaabeaef02d9f0008144d6d593c63b01
porjin2k24 <playertree333@gmail.com>
2025-10-14 09:40:55 -0300

    lebron>>>>curry

---
+++ gerenciador_tarefas/config/tarefas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    concluida = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    # Ajuda a exibir o título da tarefa no admin e em outros lugares
+    def __str__(self):
+        return self.titulo
