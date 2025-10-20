390145cc22d3a4e7da5da2d30c5e87b2225bce52
porjin2k24 <playertree333@gmail.com>
2025-10-20 08:50:09 -0300

    lebron>>>>curry

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Tarefa
+from django.http HttpResponse
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas_salvas = tarefa.objects.all()
+    contexto = {
+             "minhas_tarefas":tarefas_salvas        
+    }
+
+    return render (request, 'tarefas/lista.html',contexto)
