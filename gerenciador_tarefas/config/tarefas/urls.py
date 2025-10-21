2146fb3bb15f11e656b41a55290cd90929baaa74
porjin2k24 <playertree333@gmail.com>
2025-10-21 09:39:01 -0300

    continuando atividade

--- gerenciador_tarefas/config/tarefas/urls.py
+++ gerenciador_tarefas/config/tarefas/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path('',views.listar_tarefas,name='lista_tarefas'),
+        path('',views.listar_tarefas,name='listar_tarefas'),
+        path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
 ]
--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Tarefa
-from django.http HttpResponse
+from django.http import HttpResponse
 
 def listar_tarefas(request):
-    tarefas_salvas = tarefa.objects.all()
+    tarefas_salvas = Tarefa.objects.all()
     contexto = {
              "minhas_tarefas":tarefas_salvas        
     }
 
     return render (request, 'tarefas/lista.html',contexto)
+
+
+def detalhe_tarefa(request, tarefa_id):
+
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)    
+    
+    return render(request, 'tarefas/detalhe.html',{'tarefa': tarefa})
